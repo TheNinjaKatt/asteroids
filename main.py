853bb50638a370d354737f9eab19f9b4f2ed9db5
TheNinjaKatt <79349537+TheNinjaKatt@users.noreply.github.com>
2025-06-29 21:19:28 -0500

    larger asteroids split and speed up when hit

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collide(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
             if asteroid.collide(player):
                 print("Game over!")
